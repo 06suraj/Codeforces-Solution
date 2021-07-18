c0d251e35db0bc6d2d59bdabae21e299ae02abcb
06suraj <86453357+06suraj@users.noreply.github.com>
2021-07-18 10:07:16 +0530

    Create A - Find The Array.cpp

---
+++ Educational Codeforces Round 111 (Rated for Div. 2)/A - Find The Array.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+	// your code goes here
+	ios::sync_with_stdio(0);
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;cin>>n;cout<<ceil(sqrt(n))<<endl;
+	}
+	return 0;
+}
