bd192d93788b02a4ad785853ba37c55246a17359
06suraj <86453357+06suraj@users.noreply.github.com>
2021-08-16 09:02:38 +0530

    Update and rename Codeforces Round #736 (Div. 2).cpp to Codeforces Round #736 (Div. 2)/A - Gregor and Cryptography.cpp

---
+++ Codeforces Round #736 (Div. 2)/A - Gregor and Cryptography.cpp
@@ -0,0 +1,14 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+	// your code goes here
+	ios::sync_with_stdio(0);
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;cin>>n;cout<<2<<" "<<n-1<<endl;
+	}
+	return 0;
+}
