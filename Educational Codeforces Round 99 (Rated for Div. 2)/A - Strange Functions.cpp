40253f99af049b8aae9b60bd18209d638af67d64
06suraj <86453357+06suraj@users.noreply.github.com>
2021-08-25 22:43:49 +0530

    Create A - Strange Functions.cpp

---
+++ Educational Codeforces Round 99 (Rated for Div. 2)/A - Strange Functions.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main()
+{
+    int t;cin>>t;
+    while(t--)
+    {
+        int count=0;
+        string s;cin>>s;
+        cout<<s.size()<<endl;
+    }
+    return 0;
+}
