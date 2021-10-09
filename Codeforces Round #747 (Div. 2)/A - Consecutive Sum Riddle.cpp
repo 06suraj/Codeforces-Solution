d78c25dd74c71882375e8a41e027fffe6c274c8d
06suraj <86453357+06suraj@users.noreply.github.com>
2021-10-09 20:30:24 +0530

    Create A - Consecutive Sum Riddle.cpp

---
+++ Codeforces Round #747 (Div. 2)/A - Consecutive Sum Riddle.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+	// your code goes here
+	ios::sync_with_stdio(0);
+	int t;
+	cin>>t;
+	while(t--){
+	     long long n;cin>>n;
+	    cout<<(-1*(n-1))<<" "<<n<<endl;
+	}
+	return 0;
+}
