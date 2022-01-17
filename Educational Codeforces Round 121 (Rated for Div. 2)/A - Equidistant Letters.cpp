b7f45aa0c1814fdb30f45fa1c6f4885e3fd9f6bb
06suraj <86453357+06suraj@users.noreply.github.com>
2022-01-17 22:00:54 +0530

    Create A - Equidistant Letters.cpp

---
+++ Educational Codeforces Round 121 (Rated for Div. 2)/A - Equidistant Letters.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	ios::sync_with_stdio(0);
+	int t;
+	cin>>t;
+	while(t--){
+	    string s;cin>>s;
+	    sort(s.begin(),s.end());
+	    cout<<s<<endl;
+	}
+	return 0;
+}
